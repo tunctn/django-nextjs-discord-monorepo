1e3e649f161d149d904b64c52e14d5c03be15993
Tunç Türkmen <trkmntnc@gmail.com>
2024-08-25 12:56:57 +0200

    Update layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class">
           <div
-            className="flex h-full min-h-screen w-full flex-col border"
+            className="flex h-full min-h-screen w-full flex-col"
             suppressHydrationWarning
           >
             <Providers>
